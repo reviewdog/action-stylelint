ec7054922dcf5e066b4820a9beca6f159fceeaa4
haya14busa <haya14busa@gmail.com>
2019-09-23 06:23:32 +0000

    Always use local stylelint command and config

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,6 @@ RUN apk --update add jq && \
     rm -rf /var/lib/apt/lists/* && \
     rm /var/cache/apk/*
 
-# Install global stylelint command and configs as fallback if the workspace doesn't have stylelint.
-RUN npm install -g stylelint stylelint-config-recommended stylelint-config-recommended stylelint-config-standard
-
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,11 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-export STYLELINT_COMMAND="./node_modules/.bin/stylelint"
-if [ ! -f "${STYLELINT_COMMAND}" ]; then
-  STYLELINT_COMMAND="stylelint" # Use global one.
-  echo "Use global stylelint comamnd. Run npm install beforehand to use local command."
+if [ ! -f "$(npm bin)/stylelint" ]; then
+  npm install
 fi
 
 # Use jq to format result to include link to rule page.
-stylelint "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" -f json # \
+$(npm bin)/stylelint "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" -f json # \
   # | jq -r '{source: .[].source, warnings:.[].warnings[]} | "\(.source):\(.warnings.line):\(.warnings.column):\(.warnings.severity): \(.warnings.text) [\(.warnings.rule)](https://github.com/stylelint/stylelint/blob/master/lib/rules/\(.warnings.rule)/README.md)"' \
   # | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="stylelint" -reporter=github-pr-check -level="${INPUT_LEVEL}"
