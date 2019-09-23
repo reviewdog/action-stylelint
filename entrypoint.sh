e9bdfce9428902f63238dada78a0d1e4cbf58d64
haya14busa <haya14busa@gmail.com>
2019-09-23 06:27:31 +0000

    Run reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ if [ ! -f "$(npm bin)/stylelint" ]; then
 fi
 
 # Use jq to format result to include link to rule page.
-$(npm bin)/stylelint "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" -f json # \
-  # | jq -r '{source: .[].source, warnings:.[].warnings[]} | "\(.source):\(.warnings.line):\(.warnings.column):\(.warnings.severity): \(.warnings.text) [\(.warnings.rule)](https://github.com/stylelint/stylelint/blob/master/lib/rules/\(.warnings.rule)/README.md)"' \
-  # | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="stylelint" -reporter=github-pr-check -level="${INPUT_LEVEL}"
+$(npm bin)/stylelint "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" -f json \
+  | jq -r '{source: .[].source, warnings:.[].warnings[]} | "\(.source):\(.warnings.line):\(.warnings.column):\(.warnings.severity): \(.warnings.text) [\(.warnings.rule)](https://github.com/stylelint/stylelint/blob/master/lib/rules/\(.warnings.rule)/README.md)"' \
+  | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="stylelint" -reporter=github-pr-check -level="${INPUT_LEVEL}"
