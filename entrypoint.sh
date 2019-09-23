a9fdaa64238d4e6a601ad5132a5f60543bd09bb7
haya14busa <haya14busa@gmail.com>
2019-09-23 06:30:03 +0000

    use github-pr-review reporter

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ fi
 # Use jq to format result to include link to rule page.
 $(npm bin)/stylelint "${INPUT_STYLELINT_INPUT:-'**/*.css'}" --config="${INPUT_STYLELINT_CONFIG}" -f json \
   | jq -r '{source: .[].source, warnings:.[].warnings[]} | "\(.source):\(.warnings.line):\(.warnings.column):\(.warnings.severity): \(.warnings.text) [\(.warnings.rule)](https://github.com/stylelint/stylelint/blob/master/lib/rules/\(.warnings.rule)/README.md)"' \
-  | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="stylelint" -reporter=github-pr-check -level="${INPUT_LEVEL}"
+  | reviewdog -efm="%f:%l:%c:%t%*[^:]: %m" -name="stylelint" -reporter=github-pr-review -level="${INPUT_LEVEL}"
