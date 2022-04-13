efd6d152e007bac3bd150960a8e730459f0da2bc
Tal Tchernihovski <taltcher@gmail.com>
2022-04-13 14:39:28 +0300

    Fix for Git v2.35.2 spec changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
+git config --global --add safe.directory $GITHUB_WORKSPACE
+
 if [ ! -f "$(npm bin)/stylelint" ]; then
   npm install
 fi
